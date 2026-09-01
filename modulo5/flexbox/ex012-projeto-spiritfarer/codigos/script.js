47faa50f851e36c6cd48b2d7ce73d15dde96fe5f
dev-arthurhenrique <arthurnascimento2099@gmail.com>
2026-09-01 09:25:45 -0300

    atualizando repositorio

--- modulo5/flexbox/ex012-projeto-spiritfarer/codigos/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('burguer').addEventListener('click', b)
-function b () {
-    const menu = document.getElementById('nav')
-    if ( menu.style.display == 'block') {
-        menu.style.display = 'none'
-    } else {
-        menu.style.display = 'block'
-    }
-}
